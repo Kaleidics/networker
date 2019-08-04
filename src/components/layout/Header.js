b13d211ef975f8e2dcbe64aff1a141da3de2c53d
Eddie Chu <contact@eddiechu.me>
2019-08-03 18:45:49 -0700

    update ui for header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,6 +3,7 @@ import hero from '../../assets/logo.png';
 import ParticlesBg from '../Particles/ParticlesBg';
 
 export default class Header extends React.Component {
+    
     render() {
         return (
             <header className="header">
@@ -11,10 +12,12 @@ export default class Header extends React.Component {
                     <ParticlesBg />
                 </div>
                 <div className="header__main">
-                    <h1 className="heading-primary">the netwrkr podcast</h1>
-                    <p className="paragraph">The podcast all about <span className="u-emphasize">networking</span> and <span className="u-emphasize">networking.</span> Hosted by <span className="u-emphasize">Julian Abeleda.</span></p>
+                    <h1 className="heading-primary u-text-shadow">the netwrkr podcast</h1>
+                    <p className="paragraph u-text-shadow">
+                        The podcast all about <span className="u-emphasize">networking</span> and <span className="u-emphasize">networking.</span> Hosted by <span className="u-emphasize">Julian Abeleda.</span>
+                    </p>
                 </div>
             </header>
-        )
+        );
     }
 }
