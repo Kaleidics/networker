a351b7834b22cc67e55652d58bd128cf323c722c
Eddie Chu <contact@eddiechu.me>
2019-08-10 21:23:58 -0700

    add player

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,9 @@ import Header from "../layout/Header";
 import PodcastContainer from "../elements/PodcastContainer";
 import HostingSites from "../elements/HostingSites";
 import MainContainer from "../elements/MainContainer";
+import ReactAudioPlayer from "react-audio-player";
+import AudioCard from "audiocard";
+import AudioPlayer from "react-h5-audio-player";
 
 import Parser from "rss-parser";
 
@@ -10,27 +13,54 @@ let parser = new Parser();
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
+                   constructor(props) {
+                       super(props);
 
-        this.state = {
-            currentView: 1
-        };
-        this.setCurrentView = this.setCurrentView.bind(this);
-    }
+                       this.state = {
+                           currentView: 1,
+                           currentPodcast: ""
+                       };
+                       this.setCurrentView = this.setCurrentView.bind(this);
+                   }
 
-    setCurrentView(view) {
-        this.setState({
-            currentView: view
-        }, () => console.log("clicked", this.state.currentView));
-    }
+                   componentDidMount() {
+                       (async () => {
+                           let feed = await parser.parseURL(CORS_PROXY + "https://pinecast.com/feed/netwrkr");
+                           console.log(feed.items);
+                           this.setState({
+                               currentPodcast: feed.items[0]
+                           });
+                       })();
+                   }
 
-    render() {
-        return (
-            <div className="home">
-                <Header onClick={this.setCurrentView} selectedView={this.state.currentView}/>
-                <MainContainer selectedView={this.state.currentView} />
-            </div>
-        );
-    }
-}
+                   setCurrentView(view) {
+                       this.setState(
+                           {
+                               currentView: view
+                           },
+                           () => console.log("clicked", this.state.currentView)
+                       );
+                   }
+
+                   render() {
+                       let latestPodcastData = this.state.currentPodcast;
+                       let latestSrc = latestPodcastData.enclosure ? latestPodcastData.enclosure.url : '';
+                       let latestImage = latestPodcastData.itunes ? latestPodcastData.itunes.image : '';
+                       let latestTitle = latestPodcastData.title;
+                       console.log(latestSrc)
+
+                       return (
+                           <div className="home">
+                               <Header onClick={this.setCurrentView} selectedView={this.state.currentView} />
+                               <MainContainer selectedView={this.state.currentView} />
+                               <div className="main-audio-player">
+                                   <div className="main-audio-player__content">
+                                    <img className="main-audio-player__image" src={latestImage} />
+                                    <h3 className="main-audio-player__title" >{latestTitle}</h3>
+                                   </div>
+                                   <AudioPlayer src={latestSrc} />
+                               </div>
+                           </div>
+                       );
+                   }
+               }
