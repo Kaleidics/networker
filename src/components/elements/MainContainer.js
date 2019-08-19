bbdaf7227b59ae246108d426b22883589785da71
Eddie Chu <contact@eddiechu.me>
2019-08-18 20:18:08 -0700

    add about pages

--- src/components/elements/MainContainer.js
+++ src/components/elements/MainContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../layout/Featured';
 import AllPodcasts from '../layout/AllPodcasts';
 import AboutShow from '../layout/AboutShow';
+import AboutHost from '../layout/AboutShow';
 
 export default class MainContainer extends React.Component {
 
@@ -14,6 +15,8 @@ export default class MainContainer extends React.Component {
             content = <AllPodcasts />
         } else if (this.props.selectedView === 3) {
             content = <AboutShow />
+        } else if (this.props.selectedView ===4 ) {
+            content = <AboutHost />
         }
         
         return (
---
+++ src/components/layout/AboutHost.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class AboutHost extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2 className="heading-secondary u-margin-bottom-medium">The Host</h2>
+                <p className="paragraph u-margin-bottom-small">
+                    Julian Abeleda is an avid Tech/Silicon Valley fan. He first learned about this space by attending the ASCEND 2018 conference. This conference changed Julian's life because he met three professionals in the Tech / VC world: Justin Kan (Founder of Justin.tv, and Twitch.tv), Daniel Kan (Founder of Cruise Automation), and Emily Chang (Reporter on Bloomberg Tech). While he became interested in the tech / VC world, most of his friends thought it was boring as fuck. If you
+                    want to contact Julian Abeleda, please reach him via twitter at https://twitter.com/fromnetwrkr.
+                </p>
+            </div>
+        );
+    }
+}
